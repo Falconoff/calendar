3575a828fa21d02582d2f23f9dbee7bf863e800c
Falconoff <sokolovserega1982@gmail.com>
2020-11-13 01:25:27 +0200

    add prev/next months

--- script_cal.js
+++ script_cal.js
@@ -39,10 +39,17 @@
 
 // -------------- РАБОЧИЕ ПРОБЫ ---------------------------
 
-let showDateBlock = document.querySelector(".showDate"); // отобразим над календарём дату
+let showDatePrev = document.querySelector(".showDatePrev"); // отобразим над календарём дату предыдущего месяца
+let showDateNow = document.querySelector(".showDateNow"); // отобразим над календарём дату текщего месяца
+let showDateNext = document.querySelector(".showDateNext"); // отобразим над календарём дату следующего месяца
+
+let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 let btnPrev  = document.querySelector(".arrow__prev");   // кнопка перехода на предыдущий месяц
 let btnNext  = document.querySelector(".arrow__next");   // кнопка перехода на следущий месяц
-let dates = document.querySelector(".calendar__dates");  // поле календаря для ячеек с датами
+let monthNow = document.querySelector(".month-current__dates");  // поле календаря для ячеек с датами
+let monthPrev = document.querySelector(".month-prev__dates"); // даты предыдущего месяца
+let monthNext = document.querySelector(".month-next__dates"); // даты следующего месяца
 let btnToday  = document.querySelector(".btn-today");    // кнопка перехода на текущий месяц 
 
 	let selectedYear, yearToday;
@@ -59,11 +66,14 @@ function showCurrentMonth() {
 	selectedMonth = monthToday = today.getMonth();
 	selectedDate = dateToday = today.getDate();
 
-	showSelectedMonth(yearToday, monthToday, dateToday); // отображаем текущий месяц в календаре
+	showSelectedMonth(yearToday, monthToday, dateToday, monthNow, showDateNow); // отображаем текущий месяц в календаре
 	// console.log("today is - ", today.toDateString());// отображаем текущий месяц 
+	showSelectedMonth(yearToday, monthToday-1, dateToday, monthPrev, showDatePrev);
+	showSelectedMonth(yearToday, monthToday+1, dateToday, monthNext, showDateNext);
 }
 
 
+/*
 // ============ клик на кнопку ПРЕДЫДУЩИЙ месяц ================
 btnPrev.onclick = function() {
 	selectedMonth--;
@@ -77,6 +87,7 @@ btnNext.onclick = function() {
 	console.log("selectedMonth = ", selectedMonth);
 	showSelectedMonth(selectedYear, selectedMonth, selectedDate); // отображаем следущий месяц в календаре
 }
+*/
 
 // ============ клик на кнопку перехода на текущий месяц ================
 btnToday.onclick = function() {
@@ -87,7 +98,7 @@ btnToday.onclick = function() {
 
 // ================== отображаем выбранный месяц в календаре  =================================== //
 
-function showSelectedMonth(year, month, date) {
+function showSelectedMonth(year, month, date, dates, showDateBlock) {
 
 	dates.innerHTML = ""; // очищаем календарь от предыдущего кода
 
@@ -95,8 +106,11 @@ function showSelectedMonth(year, month, date) {
 	let myYear = myDate.getFullYear();			// год
 	let iMonth = myMonth = myDate.getMonth();	// месяц текущий и для сравнения
 
-	console.log("выбран сейчас ", myDate.toDateString());
-	showDateBlock.innerText = myDate.toDateString(); // вывод в хедере над календарем
+	// console.log("выбран сейчас ", myDate.toDateString());
+	console.log("выбран сейчас ", month, year);
+	// showDateBlock.innerText = myDate.toDateString(); // вывод в хедере над календарем
+	showDateBlock.innerText = months[month]+" "+year; // вывод в хедере над календарем
+
 
 	// =========== определяем количество дней в текущем месяце ===========
 	let iDay = 28;	// переменная для цикла
@@ -125,13 +139,13 @@ function showSelectedMonth(year, month, date) {
 
 
 
-	// let dates = document.querySelector(".calendar__dates");  объявлена выше
+	// let dates = document.querySelector(".month__dates");  объявлена выше
 	let i=1;
-	// ВЫВОДИМ ЯЧЕЙКУ предыдущего дня предыдущего месяца <div class="calendar__day disabled">дата</div>
+	// ВЫВОДИМ ЯЧЕЙКУ предыдущего дня предыдущего месяца <div class="month__day disabled">дата</div>
 	while (i <= firstDay) {
 		let prevDate = (new Date(year, month, 1 - i)).getDate(); //получаем дату -1 дня
 		let itemDay = document.createElement("div");
-		itemDay.className = "calendar__day disabled";
+		itemDay.className = "month__day disabled";
 		itemDay.innerText = prevDate;
 		// dates.insertAdjacentElement("afterBegin", itemDay);
 		dates.prepend(itemDay);
@@ -139,35 +153,39 @@ function showSelectedMonth(year, month, date) {
 		// console.log("i=", i);
 	}
 
-	// ВЫВОДИМ ЯЧЕЙКУ дня текущего месяца <div class="calendar__day">дата</div>
+	// ВЫВОДИМ ЯЧЕЙКИ дней текущего месяца <div class="month__day">дата</div>
 	let j=1;
 	while (j <= numDays) {
-		let iDate = (new Date(year, month, j)).getDate(); //получаем дату j-го дня
+		// let iDate = (new Date(year, month, j)).getDate(); //получаем дату j-го дня
 		let itemDay = document.createElement("div");
 
-if (myYear == yearToday && myMonth == monthToday && j == dateToday) {
-	itemDay.className = "calendar__day today";
-} else {
-	itemDay.className = "calendar__day";
-}
-		// itemDay.className = "calendar__day";
-		itemDay.innerText = iDate;
+		// находим совпадение с сегодняшней датой
+		if (myYear == yearToday && myMonth == monthToday && j == dateToday) {
+			itemDay.className = "month__day today";
+		} else {
+			itemDay.className = "month__day";
+		}
+		// itemDay.className = "month__day";
+		// itemDay.innerText = iDate;
+		itemDay.innerText = j;
+
 		// dates.insertAdjacentElement("afterBegin", itemDay);
 		dates.append(itemDay);
 		j++;
 	}
 
-	// ВЫВОДИМ ЯЧЕЙКУ следущего дня следущего месяца <div class="calendar__day disabled">дата</div>
+	// ВЫВОДИМ ЯЧЕЙКУ следущего дня следущего месяца <div class="month__day disabled">дата</div>
 	let diff = 6 - lastDay; // сколько надо добавить ячеек до конца недели
 	// console.log("надо добавить diff=", diff);
 
 	let d = 1;
 	while (d <= diff) {
-		
-		let nextDate = (new Date(year, month, numDays + d)).getDate(); //получаем дату next дня
+		// let nextDate = (new Date(year, month, numDays + d)).getDate(); //получаем дату next дня
 		let itemDay = document.createElement("div");
-		itemDay.className = "calendar__day disabled";
-		itemDay.innerText = nextDate;
+		itemDay.className = "month__day disabled";
+		// itemDay.innerText = nextDate;
+		itemDay.innerText = d;
+
 		dates.append(itemDay);
 		d++;
 	}
